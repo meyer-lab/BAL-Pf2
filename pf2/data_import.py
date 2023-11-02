0d75e92314436f7660f7f71cd911b1d3f4b7fa80
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2023-11-02 12:32:34 -0700

    Adds data_import, clean-up and optimization  - Adds data_import for meta-data and single-cell data import  - Cleans up docstrings for import and tensor functions  - Optimizes import for reduced memory usage  - Adds all-in-one PF2 runner for reduced memory usage

---
+++ pf2/data_import.py
@@ -0,0 +1,73 @@
+from os.path import abspath, dirname, join
+import warnings
+
+import pandas as pd
+import scanpy as sc
+
+REPO_PATH = dirname(abspath(__file__))
+
+
+def import_meta():
+    """
+    Imports meta-data.
+
+    Returns:
+         meta (pd.DataFrame): patient metadata
+    """
+    meta = pd.read_csv(
+        join(
+            REPO_PATH,
+            'data',
+            '04_external.csv'
+        ),
+        index_col=0
+    )
+    meta = meta.loc[meta.loc[:, 'BAL_performed'], :]
+
+    return meta
+
+
+def import_data(high_variance_genes=True, sum_one=False, log_transform=False,
+                normalize=True, size='m'):
+    """
+    Imports and preprocesses single-cell data.
+
+    Parameters:
+        high_variance_genes (bool, default:True): use only high-variance genes
+        sum_one (bool, default:False): sums each gene to 1 across cells
+        log_transform (bool, default:True): log transform data
+        normalize (bool, default:True): zero mean, unit variance genes
+        size (str, default:'m'): size of dataset to use; must be one of 'small',
+            'medium', 'large', 's', 'm', or 'l'
+    """
+    if size not in ['small', 'medium', 'large', 's', 'm', 'l']:
+        size = 'm'
+        warnings.warn("'size' parameter not recognized; defaulting to 'medium'")
+
+    if size in ['small', 's']:
+        fp = 'v4_11integrated_cleaned_small.h5ad'
+    elif size in ['medium', 'm']:
+        fp = 'v4_11integrated_cleaned_medium.h5ad'
+    else:
+        fp = 'v4_11_integrated_cleaned.h5ad'
+
+    adata = sc.read_h5ad(
+        join(
+            REPO_PATH,
+            'data',
+            fp
+        ),
+    )
+    if high_variance_genes:
+        adata = adata[:, adata.var.loc[:, 'highly_variable']]
+
+    if sum_one:
+        adata.X /= adata.X.sum(axis=0)
+
+    if log_transform:
+        sc.pp.log1p(adata)
+
+    if normalize:
+        sc.pp.scale(adata)
+
+    return adata
--- pf2/tensor.py
+++ pf2/tensor.py
@@ -1,95 +1,116 @@
-from os.path import abspath, dirname, join
+import gc
 
+from data_import import import_data
 import numpy as np
-import scanpy as sc
-from scipy.stats import zscore
+import pandas as pd
 from tensorly.decomposition import parafac2
 
-
 OPTIMAL_RANK = 40
-REPO_PATH = dirname(abspath(__file__))
 
 
-def calc_R2X(projected, data):
-    """ Calculate the top and bottom part of R2X formula separately """
+def calc_r2x(projected, data):
+    """
+    Calculate the top and bottom part of R2X formula separately.
+
+    Parameters:
+        projected (np.ndarray): reconstructed tensor
+        data (np.ndarray): original tensor
+
+    Returns:
+        v_top (float): variance explained
+        v_bottom (float): total variance in tensor
+    """
     v_top = np.linalg.norm(projected - data) ** 2.0
     v_bottom = np.linalg.norm(data) ** 2.0
     return v_top, v_bottom
 
 
 def get_variance_explained(pf2, tensor):
-    """Computes variance explained"""
+    """
+    Computes variance explained.
+
+    Parameters:
+        pf2 (tensorly.Parafac2Tensor): PF2 factorization
+        tensor (np.ndarray): original dataset
+
+    Returns:
+        R2X (float): proportion of variance explained via PF2
+    """
     top, bottom = 0, 0
     projected = pf2.to_tensor()
     for index, matrix in enumerate(tensor):
         projected_matrix = projected[index, :matrix.shape[0], :]
-        slice_var = calc_R2X(projected_matrix, matrix)
+        slice_var = calc_r2x(projected_matrix, matrix)
         top += slice_var[0]
         bottom += slice_var[1]
 
     return 1 - top / bottom
 
 
-def import_data(high_variance_genes=True, sum_one=False, log_transform=True,
-                normalize=True):
-    """Imports and preprocesses data."""
-    adata = sc.read_h5ad(
-        join(
-            REPO_PATH,
-            'data',
-            'trimmed_10f.h5ad'
-        ),
-        backed='r+'
-    )
-    if high_variance_genes:
-        adata = adata[:, adata.var.loc[:, 'highly_variable']]
-
-    data = adata.to_df()
-    data.index = adata.obs.loc[:, 'library_id']
-
-    if sum_one:
-        data /= data.sum(axis=0)
-
-    if log_transform:
-        data = (data * 1E3) + 1
-        data = np.log10(data)
-
-    if normalize:
-        data[:] = zscore(data, axis=0)
-
-    return data
-
-
 def build_tensor(data, drop_low=100):
-    """Builds PARAFAC2 tensor."""
-    patients = list(data.index.unique())
+    """
+    Builds PARAFAC2 tensor.
+
+    Parameters:
+        data (AnnData): single cell data
+        drop_low (int, default:100): drops patients with fewer cells than
+            drop_low
+
+    Returns:
+        tensor (np.ndarray): PARAFAC2 tensor
+        labels (pd.DataFrame): maps tensor indices to patient and sample IDs
+    """
+    sample_ids = pd.Series((data.obs.loc[:, 'sample_id'].unique()))
     tensor = []
+    labels = pd.DataFrame(
+        columns=['patient_id', 'sample_id'],
+        dtype=object
+    )
 
-    if drop_low:
-        kept_patients = []
-
-    for patient in patients:
-        matrix = data.loc[
-            data.index == patient,
+    for index, sample_id in enumerate(sample_ids):
+        matrix = data[
+            data.obs.loc[:, 'sample_id'] == sample_id,
             :
         ]
+        data = data[
+            ~(data.obs.loc[:, 'sample_id'] == sample_id),
+            :
+        ]
+        gc.collect()
+
         if drop_low:
             if matrix.shape[0] >= drop_low:
-                tensor.append(matrix)
-                kept_patients.append(patient)
+                tensor.append(matrix.X)
+                labels.loc[index, :] = [
+                    matrix.obs.loc[:, 'patient_id'].iloc[0],
+                    matrix.obs.loc[:, 'sample_id'].iloc[0]
+                ]
         else:
-            tensor.append(matrix.values)
+            tensor.append(matrix.X)
+            labels.loc[index, :] = [
+                matrix.obs.loc[:, 'patient_id'].iloc[0],
+                matrix.obs.loc[:, 'sample_id'].iloc[0]
+            ]
 
-    if drop_low:
-        return tensor, kept_patients
-    else:
-        return tensor, patients
+    del data
+    gc.collect()
+
+    return tensor, labels
 
 
-def run_parafac2(data, rank=OPTIMAL_RANK):
-    """Runs PARAFAC2 on the provided data."""
+def run_parafac2(tensor, rank=OPTIMAL_RANK):
+    """
+    Runs PARAFAC2 on the provided data.
+
+    Parameters:
+        tensor (np.ndarray): PARAFAC2 tensor
+        rank (int, default:DEFAULT_RANK): rank of PF2 decomposition
+
+    Returns:
+        pf2 (tensorly.Parafac2Tensor): PF2 factorization
+    """
     pf2 = parafac2(
-        data,
+        tensor,
         rank=rank,
         init='svd',
         svd='randomized_svd',
@@ -99,12 +120,66 @@ def run_parafac2(data, rank=OPTIMAL_RANK):
     return pf2
 
 
-def main():
-    data = import_data()
-    tensor, patients = build_tensor(data)
-    pf2 = run_parafac2(tensor)
-    print(get_variance_explained(pf2, tensor))
+def pf2_low_memory(data_params=None, drop_low=100, rank=OPTIMAL_RANK):
+    """
+    Combines data import, tensor building, PF2 for reduced memory usage.
 
+    Parameters:
+        data_params (dict, default:None): parameters to pass to import_data
+        drop_low (int, default:100): drops patients with fewer cells than
+            drop_low
+        rank (int, default:DEFAULT_RANK): rank of PF2 decomposition
 
-if __name__ == '__main__':
-    main()
+    Returns:
+        pf2 (tensorly.Parafac2Tensor): PF2 factorization
+        labels (pd.DataFrame): maps tensor indices to patient and sample IDs
+    """
+    if data_params is None:
+        data_params = {}
+
+    data = import_data(**data_params)
+    sample_ids = pd.Series((data.obs.loc[:, 'sample_id'].unique()))
+    tensor = []
+    labels = pd.DataFrame(
+        columns=['patient_id', 'sample_id'],
+        dtype=object
+    )
+
+    for index, sample_id in enumerate(sample_ids):
+        matrix = data[
+                 data.obs.loc[:, 'sample_id'] == sample_id,
+                 :
+                 ]
+        data = data[
+               ~(data.obs.loc[:, 'sample_id'] == sample_id),
+               :
+               ]
+        gc.collect()
+
+        if drop_low:
+            if matrix.shape[0] >= drop_low:
+                tensor.append(matrix.X)
+                labels.loc[index, :] = [
+                    matrix.obs.loc[:, 'patient_id'].iloc[0],
+                    matrix.obs.loc[:, 'sample_id'].iloc[0]
+                ]
+        else:
+            tensor.append(matrix.X)
+            labels.loc[index, :] = [
+                matrix.obs.loc[:, 'patient_id'].iloc[0],
+                matrix.obs.loc[:, 'sample_id'].iloc[0]
+            ]
+
+    del data
+    gc.collect()
+
+    pf2 = parafac2(
+        tensor,
+        rank=rank,
+        init='svd',
+        svd='randomized_svd',
+        normalize_factors=True,
+        tol=1E-6
+    )
+
+    return pf2, labels
