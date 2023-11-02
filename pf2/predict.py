b6f3afd34d9540d709f795b61b723537882269f4
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2023-11-02 12:34:31 -0700

    Updates predict interface to match tensor changes

--- pf2/predict.py
+++ pf2/predict.py
@@ -1,14 +1,33 @@
+from os.path import abspath, dirname, join
+
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.model_selection import StratifiedKFold
 
-
+CACHE_DIR = join(dirname((abspath(__file__))), 'output')
+OPTIMAL_RANK = 40
+REPO_PATH = dirname(abspath(__file__))
 SKF = StratifiedKFold(n_splits=5)
 
 
 def predict_mortality(data, labels, proba=False):
-    """Predicts mortality via cross-validation."""
+    """
+    Predicts mortality via cross-validation.
+
+    Parameters:
+        data (pd.DataFrame): data to predict
+        labels (pd.Series): labels to predict from data
+        proba (bool, default:False): return probability of prediction
+
+    Returns:
+        if proba:
+            probabilities (pd.Series): probability of mortality for patients;
+                shares index with labels
+        else:
+            accuracy (float): prediction accuracy
+            coefficients (np.array): LR model coefficients
+    """
     l1_ratios = np.linspace(0, 1, 11)
     model = LogisticRegressionCV(
         l1_ratios=l1_ratios,
@@ -31,56 +50,13 @@ def predict_mortality(data, labels, proba=False):
             max_iter=100000
         )
         for train_index, test_index in SKF.split(data, labels):
-            train_data, train_labels = data.iloc[train_index, :], labels.iloc[train_index]
+            train_data = data.iloc[train_index, :]
+            train_labels = labels.iloc[train_index]
             test_data = data.iloc[test_index]
             lr_model.fit(train_data, train_labels)
             predicted = lr_model.predict_proba(test_data)
-            probabilities.loc[test_data.index] = predicted[:, 1]
+            probabilities.iloc[test_index] = predicted[:, 1]
         return probabilities
     else:
         scores = np.mean(list(model.scores_.values())[0], axis=0)
         return np.max(scores), model.coef_[0]
-
-
-def main():
-    patients = pd.read_csv(
-        f'output/10_fold_unscaled/patient_factors_rank_2.csv',
-        index_col=0
-    ).index
-    conversions = pd.read_csv(
-        f'data/SCRIPT_pt_bal_anonymized_ids_external.csv',
-        index_col=0
-    )
-
-    in_conversions = patients.isin(conversions.index)
-    conversions = conversions.loc[
-        patients[in_conversions],
-        'External patient ID'
-    ]
-
-    meta = pd.read_csv(
-        f'data/Carpediem/CarpeDiem_dataset.csv',
-        index_col=0
-    )
-    meta = meta.set_index('Patient_id', drop=True)
-    meta = meta.loc[~meta.index.duplicated(), :]
-    in_meta = conversions.isin(meta.index)
-
-    labels = meta.loc[
-        conversions.loc[in_meta],
-        'Binary_outcome'
-    ]
-
-    data = pd.read_csv(
-        'Patient_factor.csv',
-        index_col=0
-    )
-    data = data.loc[in_conversions, :].loc[in_meta, :]
-    data = data / data.max(axis=0)
-
-    score, model = predict_mortality(data, labels)
-    print(score)
-
-
-if __name__ == '__main__':
-    main()
