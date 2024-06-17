7b1da2b86d7f61ccabb6fe80c5802e6859eaa351
Jackson Chin <JacksonLymanChin@gmail.com>
2024-06-17 16:41:59 -0700

    Adds normalization, recursive feature elimination to predict

--- pf2/predict.py
+++ pf2/predict.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import RFECV
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import scale
 
-SKF = StratifiedKFold(n_splits=5)
+SKF = StratifiedKFold(n_splits=10)
 
 
 def predict_mortality(data, labels, proba=False):
@@ -23,35 +26,49 @@ def predict_mortality(data, labels, proba=False):
             accuracy (float): prediction accuracy
             coefficients (np.array): LR model coefficients
     """
+    data.loc[:, :] = scale(data)
     l1_ratios = np.linspace(0, 1, 11)
+    rfe_model = LogisticRegression()
+
+    rfe_cv = RFECV(rfe_model, step=1, cv=SKF)
+    rfe_cv.fit(data, labels)
+    data = data.loc[:, rfe_cv.support_]
+
     model = LogisticRegressionCV(
+        Cs=11,
         l1_ratios=l1_ratios,
         solver="saga",
         penalty="elasticnet",
         n_jobs=1,
         cv=SKF,
-        max_iter=100000,
-        multi_class="ovr",
+        max_iter=100000
     )
     model.fit(data, labels)
 
+    probabilities = pd.Series(0, dtype=float, index=data.index)
+    lr_model = LogisticRegression(
+        penalty="elasticnet",
+        solver="saga",
+        l1_ratio=model.l1_ratio_[0],
+        C=model.C_[0],
+        max_iter=100000,
+    )
+    for train_index, test_index in SKF.split(data, labels):
+        train_data = data.iloc[train_index, :]
+        train_labels = labels.iloc[train_index]
+        test_data = data.iloc[test_index]
+        lr_model.fit(train_data, train_labels)
+        predicted = lr_model.predict_proba(test_data)
+        probabilities.iloc[test_index] = predicted[:, 1]
+
+    lr_model.fit(data, labels)
+
     if proba:
-        probabilities = pd.Series(0, dtype=float, index=data.index)
-        lr_model = LogisticRegression(
-            penalty="elasticnet",
-            solver="saga",
-            l1_ratio=model.l1_ratio_[0],
-            C=model.C_[0],
-            max_iter=100000,
-        )
-        for train_index, test_index in SKF.split(data, labels):
-            train_data = data.iloc[train_index, :]
-            train_labels = labels.iloc[train_index]
-            test_data = data.iloc[test_index]
-            lr_model.fit(train_data, train_labels)
-            predicted = lr_model.predict_proba(test_data)
-            probabilities.iloc[test_index] = predicted[:, 1]
         return probabilities
     else:
-        scores = np.mean(list(model.scores_.values())[0], axis=0)
-        return np.max(scores), model.coef_[0]
+        predicted = probabilities.round().astype(int)
+        coefficients = pd.Series(
+            lr_model.coef_[0],
+            index=np.argwhere(rfe_cv.support_).flatten() + 1
+        )
+        return accuracy_score(labels, predicted), coefficients
