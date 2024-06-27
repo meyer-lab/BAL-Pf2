17ecf5d0720a64444aa45d963c014f5743cd17b9
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-06-27 14:38:36 -0700

    Add big summer files

---
+++ pf2/figures/figureBS_S1.py
@@ -0,0 +1,18 @@
+"""Figure BS_S1: Description"""
+
+from anndata import read_h5ad
+from .common import subplotLabel, getSetup
+from ..tensor import correct_conditions
+
+
+def makeFigure():
+    ax, f = getSetup((8, 12), (2, 1))
+    subplotLabel(ax)
+
+    X = read_h5ad("/opt/northwest_bal/full_fitted.h5ad", backed="r")
+
+    X.uns["Pf2_A"] = correct_conditions(X)  
+    condition_factors = X.uns["Pf2_A"]
+    
+
+    return f
