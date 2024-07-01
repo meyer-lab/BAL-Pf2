f93750da24b5ace4e35e8e87ad7dcc00b177ab6a
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-07-01 14:16:18 -0700

    Fix import file

--- pf2/figures/figureBS_S1.py
+++ pf2/figures/figureBS_S1.py
@@ -1,6 +1,6 @@
 """Figure BS_S1: Description"""
 
-from anndata import read_h5ad
+import anndata
 from .common import subplotLabel, getSetup
 from ..tensor import correct_conditions
 
@@ -9,7 +9,7 @@ def makeFigure():
     ax, f = getSetup((8, 12), (2, 1))
     subplotLabel(ax)
 
-    X = read_h5ad("/opt/northwest_bal/full_fitted.h5ad", backed="r")
+    X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
 
     X.uns["Pf2_A"] = correct_conditions(X)  
     condition_factors = X.uns["Pf2_A"]
