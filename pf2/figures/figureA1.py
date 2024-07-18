50cadcc7a0dd582abff6775cfc96633ab365163e
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-07-17 17:01:51 -0700

    Create more categories

--- pf2/figures/figureA1.py
+++ pf2/figures/figureA1.py
@@ -11,6 +11,8 @@ from pf2.figures.commonFuncs.plotFactors import (
 )
 from pf2.figures.commonFuncs.plotPaCMAP import plot_labels_pacmap
 from pf2.data_import import combine_cell_types, add_obs
+from pf2.figures.commonFuncs.plotGeneral import bal_combine_bo_covid
+import pandas as pd
 
 
 def makeFigure():
@@ -24,8 +26,14 @@ def makeFigure():
     plot_gene_factors(X, ax[2])
     plot_labels_pacmap(X, "cell_type", ax[3])
     add_obs(X, "patient_category")
-    plot_labels_pacmap(X, "patient_category", ax[4])
-
+    add_obs(X, "binary_outcome")
+    
+    df = X.obs[["patient_category", "binary_outcome"]].reset_index(drop=True)
+    
+    df = bal_combine_bo_covid(df)
+    X.obs["Status"] = df["Status"].to_numpy()
+    
+    plot_labels_pacmap(X, "Status", ax[4])
     combine_cell_types(X)
     plot_labels_pacmap(X, "combined_cell_type", ax[5])
 
