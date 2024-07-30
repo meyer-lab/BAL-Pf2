2f712e20bf5b02e138336e3c4c0d3b8f46e8cf69
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-07-30 16:13:23 -0700

    Add pair for cond and gene factors

---
+++ pf2/figures/figureA8.py
@@ -0,0 +1,53 @@
+"""
+Figure 5f_j
+"""
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from matplotlib.axes import Axes
+import anndata
+from .common import subplotLabel, getSetup
+from .commonFuncs.plotPaCMAP import plot_wp_pacmap
+from ..tensor import correct_conditions
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    ax, f = getSetup((8, 8), (3, 3))
+    subplotLabel(ax)
+    
+    X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
+    X.uns["Pf2_A"] = correct_conditions(X)
+
+    plot_pair_gene_factors(X, 22, 28, ax[0])
+    plot_pair_cond_factors(X, 1, 5, ax[1])
+
+    return f
+
+
+
+
+def plot_pair_gene_factors(X: anndata.AnnData, cmp1: int, cmp2: int, ax: Axes):
+    """Plots two gene components weights"""
+    cmpWeights = np.concatenate(
+        ([X.varm["Pf2_C"][:, cmp1 - 1]], [X.varm["Pf2_C"][:, cmp2 - 1]])
+    )
+    df = pd.DataFrame(
+        data=cmpWeights.transpose(), columns=[f"Cmp. {cmp1}", f"Cmp. {cmp2}"]
+    )
+    sns.scatterplot(data=df, x=f"Cmp. {cmp1}", y=f"Cmp. {cmp2}", ax=ax)
+    ax.set(title="Gene Factors")
+    
+    
+
+def plot_pair_cond_factors(X: anndata.AnnData, cmp1: int, cmp2: int, ax: Axes):
+    """Plots two gene components weights"""
+    cmpWeights = np.concatenate(
+        ([X.uns["Pf2_A"][:, cmp1 - 1]], [X.uns["Pf2_A"][:, cmp2 - 1]])
+    )
+    df = pd.DataFrame(
+        data=cmpWeights.transpose(), columns=[f"Cmp. {cmp1}", f"Cmp. {cmp2}"]
+    )
+    sns.scatterplot(data=df, x=f"Cmp. {cmp1}", y=f"Cmp. {cmp2}", ax=ax)
+    ax.set(title="Condition Factors")
