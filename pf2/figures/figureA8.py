09be190818d42e98bfa59e7d8c7d0a722670b30d
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-08-01 10:41:00 -0700

    Partial correlation between components

--- pf2/figures/figureA8.py
+++ pf2/figures/figureA8.py
@@ -19,43 +19,44 @@ def makeFigure():
     
     X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
     X.uns["Pf2_A"] = correct_conditions(X)
+
     
-    plot_pair_gene_factors(X, 25, 49, ax[0])
-    plot_pair_cond_factors(X, 25, 29, ax[1])
-    plot_pair_wp(X, 25, 49, ax[2], frac=.001)
-    plot_pair_wp_pacmap(X, 24, 49, ax[3])
+    # plot_pair_gene_factors(X, 28, 45, ax[0])
+    # plot_pair_cond_factors(X, 28, 45, ax[1])
+    # plot_pair_wp(X, 28, 45, ax[2], frac=.001)
+    # plot_pair_wp_pacmap(X, 28, 45, ax[3])
     
+    # plot_pair_gene_factors(X, 25, 49, ax[4])
+    # plot_pair_cond_factors(X, 25, 29, ax[5])
+    # plot_pair_wp(X, 25, 49, ax[6], frac=.001)
+    # plot_pair_wp_pacmap(X, 25, 49, ax[7])
     
-    plot_pair_gene_factors(X, 28, 45, ax[4])
-    plot_pair_cond_factors(X, 28, 45, ax[5])
-    plot_pair_wp(X, 28, 45, ax[6], frac=.001)
-    plot_pair_wp_pacmap(X, 38, 45, ax[7])
+    # plot_pair_gene_factors(X, 2, 5, ax[8])
+    # plot_pair_cond_factors(X, 2, 5, ax[9])
+    # plot_pair_wp(X, 2, 5, ax[10], frac=.001)
+    # # plot_pair_wp_pacmap(X, 2, 5, ax[11])
     
-    ax[0].set(xlim=(-.05, .15), ylim=(-.05, .15))
-    ax[1].set(xlim=(-1, 10), ylim=(-1, 20))
-    ax[2].set(xlim=(-.15, .15), ylim=(-.1, .15))
+    # plot_pair_gene_factors(X, 45, 47, ax[12])
+    # plot_pair_cond_factors(X, 45, 47, ax[13])
+    # plot_pair_wp(X, 45, 47, ax[14], frac=.001)
+
     
+    # ax[0].set(xlim=(-.05, .15), ylim=(-.05, .15))
+    # ax[1].set(xlim=(-1, 10), ylim=(-1, 20))
+    # ax[2].set(xlim=(-.15, .15), ylim=(-.1, .15))
    
-    ax[4].set(xlim=(-.3, .4), ylim=(-.4, .4))
-    ax[5].set(xlim=(-1, 20), ylim=(-1, 20))
-    ax[6].set(xlim=(-.4, .4), ylim=(-.4, .2))
+    # ax[4].set(xlim=(-.3, .4), ylim=(-.4, .4))
+    # ax[5].set(xlim=(-1, 20), ylim=(-1, 20))
+    # ax[6].set(xlim=(-.4, .4), ylim=(-.4, .2))
     
-    plot_y_x_line(ax[0], pos=True)
-    plot_y_x_line(ax[1], pos=True)
-    plot_y_x_line(ax[2], pos=False)
+    # plot_y_x_line(ax[0], pos=True)
+    # plot_y_x_line(ax[1], pos=True)
+    # plot_y_x_line(ax[2], pos=False)
     
-    plot_y_x_line(ax[4], pos=True)
-    plot_y_x_line(ax[5], pos=True)
-    plot_y_x_line(ax[6], pos=False)
-    
-    
-    # ax[0].set(xlim=(-.5, .15), ylim=(-.5, .15))
-    # ax[1].set(xlim=(-1, 20), ylim=(-1, 20))
-    
-    
-    # ax[0].set(xticks=[-.4, 0, 4], yticks=[-.4, 0, .4])
-    # # ax[1].set(xticks=[-1, 5, 10, 15, 20, 25], yticks=[-1, 5, 10, 15, 20, 25])
-    # ax[1].set(xlim=(-2, 25), ylim=(-2, 25))
+    # plot_y_x_line(ax[4], pos=True)
+    # plot_y_x_line(ax[5], pos=True)
+    # plot_y_x_line(ax[6], pos=False)
+
 
     return f
 
