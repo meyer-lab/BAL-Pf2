f402f3ca45086a8a4d2e3111198d49f9e71af71b
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-08-19 11:51:05 -0700

    Subset Anndata file itself

--- pf2/figures/figureA10.py
+++ pf2/figures/figureA10.py
@@ -8,6 +8,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from ..tensor import correct_conditions
 from .common import getSetup
+import seaborn as sns
+import scanpy as sc
+from pf2.data_import import combine_cell_types, add_obs
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
@@ -20,19 +23,25 @@ def makeFigure():
         data=X.uns["Pf2_A"],
         columns=[f"Cmp. {i}" for i in np.arange(1, X.uns["Pf2_A"].shape[1] + 1)],
     )
+    combine_cell_types(X)
+
+    cmp = 3
     
     wprojs = X.obsm["weighted_projections"]
-    thres_value = 5
+    # thres_value = 1
+    thres_value= 99
     threshold = np.percentile(wprojs, thres_value, axis=0) 
-
-    wprojs_subset = np.zeros((wprojs[wprojs[:, 0] < threshold[0], :].shape[0], wprojs.shape[1]))
-
-    for i in range(len(threshold)):
-        wprojs_subset[:, i] = wprojs[wprojs[:, i] < threshold[i], i]
+    # subsetX = X[wprojs[:, cmp-1] < threshold[cmp-1], :]
+    
+    subsetX = X[wprojs[:, cmp-1] > threshold[cmp-1], :]
+    
+    sns.violinplot(subsetX.obsm["weighted_projections"][:, cmp-1], ax=ax[0])
+    
+    # sc.tl.rank_genes_groups(subsetX, "combined_cell_type", method="t-test")
+    # sc.pl.rank_genes_groups(subsetX, n_genes=30, save="RGG.png")
         
-        
-    print
-
+    
+    
     return f
 
     
