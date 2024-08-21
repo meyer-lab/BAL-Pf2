fcefb138fe4b5848d2726abd99a375d1e23aaf2e
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-08-21 09:57:07 -0700

    Separate into two functions

--- pf2/figures/figureA10.py
+++ pf2/figures/figureA10.py
@@ -12,24 +12,29 @@ import seaborn as sns
 import scanpy as sc
 from pf2.data_import import combine_cell_types, add_obs
 from .commonFuncs.plotPaCMAP import plot_wp_pacmap, plot_wp_per_celltype 
+from pf2.figures.commonFuncs.plotGeneral import rotate_xaxis, bal_combine_bo_covid
+from ..data_import import add_obs, condition_factors_meta, combine_cell_types
+
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     ax, f = getSetup((8, 8), (3, 3))
 
     X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
+    add_obs(X, "binary_outcome")
+    add_obs(X, "patient_category")
    
 
-    condition_factors_df = pd.DataFrame(
-        data=X.uns["Pf2_A"],
-        columns=[f"Cmp. {i}" for i in np.arange(1, X.uns["Pf2_A"].shape[1] + 1)],
-    )
+    # condition_factors_df = pd.DataFrame(
+    #     data=X.uns["Pf2_A"],
+    #     columns=[f"Cmp. {i}" for i in np.arange(1, X.uns["Pf2_A"].shape[1] + 1)],
+    # )
     combine_cell_types(X)
 
     cmp1 = 26
     
     wprojs = X.obsm["weighted_projections"]
-    thres_value= 99.5
+    thres_value= 99
     threshold1 = np.percentile(wprojs, thres_value, axis=0) 
     # subsetX = X[wprojs[:, cmp-1] < threshold[cmp-1], :]
     #  subsetX = X[wprojs[:, cmp-1] > threshold[cmp-1], :]
@@ -37,7 +42,7 @@ def makeFigure():
     X.obs[f"Cmp{cmp1}"] = idx
     
     cmp2 = 3
-    thres_value= 99.5
+    thres_value= 99
     threshold = np.percentile(wprojs, thres_value, axis=0) 
     idx = wprojs[:, cmp2-1] > threshold[cmp2-1]
     X.obs[f"Cmp{cmp2}"] = idx
@@ -48,14 +53,46 @@ def makeFigure():
     X.obs["Both"] = idx
     
     
-        
-    print(X)
+        # print(X)
     
      
-    plot_avegene_per_status_sep(
+    # df = plot_avegene_per_status_both(
+    # X, 
+    # "KIF20A",
+    # ax[0])
+    # df = plot_avegene_per_status_sep(
+    # X, 
+    # "KIF20A",
+    # ax[1])
+    df = plot_avegene_per_status_both(
     X, 
-    "KIF20A",
+    "FAM111B",
     ax[0])
+    df = plot_avegene_per_status_sep(
+    X, 
+    "FAM111B",
+    ax[1])
+    df = plot_avegene_per_status_both(
+    X, 
+    "CD3E",
+    ax[4])
+    df = plot_avegene_per_status_sep(
+    X, 
+    "CD3E",
+    ax[5])
+    
+    
+    
+    print(X[X.obs["Cmp3"] == True])
+    
+    plot_cell_count(X[X.obs["Cmp3"] == True], ax[6])
+    ax[6].set(title="Cmp3")
+    plot_cell_count(X[X.obs["Cmp26"] == True], ax[7])
+    ax[7].set(title="Cmp26")
+    plot_cell_count(X[X.obs["Both"] == True], ax[8])
+    ax[8].set(title="Both")
+        
+
     
     
     # plot_avegene_per_status(
@@ -94,7 +131,7 @@ def makeFigure():
     
     
     
-def plot_avegene_per_status(
+def plot_avegene_per_status_both(
     X: anndata.AnnData,
     gene: str,
     ax,
@@ -119,13 +156,8 @@ def plot_avegene_per_status(
     dataDF.loc[(dataDF["Cmp3"] == True) & (dataDF["Cmp26"] == True), "Label"] = "Both"
     dataDF.loc[((dataDF["Cmp3"] == True) & (dataDF["Cmp26"] == False), "Label")] = "Cmp3"
     dataDF.loc[(dataDF["Cmp3"] == False) & (dataDF["Cmp26"] == True), "Label"] = "Cmp26"
-    
-    print(dataDF)
+
     dataDF = dataDF.dropna(subset="Label")
-    print(dataDF)
-    # # dataDF.loc[dataDF["Cmp3"] == False, "Label"] = "Neither"
-    # dataDF.loc[dataDF["Cmp26"] == False, "Label"] = "Neither"
-    
     
     df = pd.melt(
         dataDF, id_vars=["Label", "Condition"], value_vars=gene
@@ -134,7 +166,6 @@ def plot_avegene_per_status(
     df = df.groupby(["Label", "Gene", "Condition"], observed=False).mean()
     df = df.rename(columns={"Value": "Average Gene Expression"}).reset_index()
     
-    print(df)
     
     sns.boxplot(
         data=df.loc[df["Gene"] == gene],
@@ -144,6 +175,8 @@ def plot_avegene_per_status(
         showfliers=False,
     )
     ax.set(ylabel=f"Average {gene}")
+    
+    return df
     
   
 
@@ -168,19 +201,8 @@ def plot_avegene_per_status_sep(
     
     dataDF["Cmp3"] = genesV.obs["Cmp3"].values
     dataDF["Cmp26"] = genesV.obs["Cmp26"].values
-    # dataDF["Both"] = genesV.obs["Both"].values
-    
-    # dataDF.loc[(dataDF["Cmp3"] == True) & (dataDF["Cmp26"] == True), "Label"] = "Both"
-    # dataDF.loc[((dataDF["Cmp3"] == True) & (dataDF["Cmp26"] == False), "Label")] = "Cmp3"
-    # dataDF.loc[(dataDF["Cmp3"] == False) & (dataDF["Cmp26"] == True), "Label"] = "Cmp26"
-    
-    # print(dataDF)
-    # dataDF = dataDF.dropna(subset="Label")
-    # print(dataDF)
-    # # dataDF.loc[dataDF["Cmp3"] == False, "Label"] = "Neither"
-    # dataDF.loc[dataDF["Cmp26"] == False, "Label"] = "Neither"
-    
-    
+
+
     df1 = pd.melt(
         dataDF, id_vars=["Condition", "Cmp3"], value_vars=gene
     ).rename(columns={"variable": "Gene", "value": "Value"})
@@ -189,12 +211,10 @@ def plot_avegene_per_status_sep(
     df1 = df1.rename(columns={"Value": "Average Gene Expression"}).reset_index()
     
     df1 = df1.loc[df1["Cmp3"] == True]
-
     df1["Label"] = "Cmp3"
-    
+      
 
     
-       
     df2 = pd.melt(
         dataDF, id_vars=["Condition", "Cmp26"], value_vars=gene
     ).rename(columns={"variable": "Gene", "value": "Value"})
@@ -208,10 +228,6 @@ def plot_avegene_per_status_sep(
     df = pd.concat([df1, df2])
     df = df.drop(columns=["Cmp3", "Cmp26"]).dropna()
     
-    print(df)
-    
-  
-    
     sns.boxplot(
         data=df,
         x="Label",
@@ -221,7 +237,22 @@ def plot_avegene_per_status_sep(
     )
     ax.set(ylabel=f"Average {gene}")
     
+    
+    return df
+    
   
+  
+          
+def plot_cell_count(X: anndata.AnnData, ax, cond: str = "sample_id",
+                    status1: str = "binary_outcome", status2: str = "patient_category"):
+    """Plots overall cell count."""
+    df = X.obs[[cond, status1, status2]].reset_index(drop=True)
+    
+    df = bal_combine_bo_covid(df)
+    dfCond = df.groupby([cond, "Status"], observed=True).size().reset_index(name="Cell Count")
+
+    sns.barplot(data=dfCond, x="Status", y="Cell Count", hue="Status", ax=ax)
+    rotate_xaxis(ax)
 
     
     
