724a53ff9b6324ee2fd2b57f4a274d38ef5a2d6c
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-08-28 10:58:35 -0700

    Add files investigating RF and FMS

---
+++ pf2/figures/figureA10.py
@@ -0,0 +1,147 @@
+"""
+Figure S4
+"""
+
+import numpy as np
+import scanpy as sc
+import anndata
+from matplotlib.axes import Axes
+from tlviz.factor_tools import factor_match_score as fms
+from tensorly.cp_tensor import CPTensor
+import seaborn as sns
+import pandas as pd
+from .common import subplotLabel, getSetup
+from pf2.tensor import pf2
+from pf2.data_import import import_data
+# from ..imports import import_thomson
+
+
+def makeFigure():
+    ax, f = getSetup((6, 3), (1, 2))
+    subplotLabel(ax)
+
+    # X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
+    X = import_data()
+    percentList = np.arange(0.0, 10, 0.5)
+    plot_fms_percent_drop(X, ax[0], percentList=percentList, runs=3)
+
+    ranks = list([1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70])
+    plot_fms_diff_ranks(X, ax[1], ranksList=ranks, runs=3)
+
+    return f
+
+
+def calculateFMS(A: anndata.AnnData, B: anndata.AnnData):
+    """Calculates FMS between 2 factors"""
+    factors = [A.uns["Pf2_A"], A.uns["Pf2_B"], A.varm["Pf2_C"]]
+    A_CP = CPTensor(
+        (
+            A.uns["Pf2_weights"],
+            factors,
+        )
+    )
+
+    factors = [B.uns["Pf2_A"], B.uns["Pf2_B"], B.varm["Pf2_C"]]
+    B_CP = CPTensor(
+        (
+            B.uns["Pf2_weights"],
+            factors,
+        )
+    )
+
+    return fms(A_CP, B_CP, consider_weights=False, skip_mode=1)  # type: ignore
+
+
+def plot_fms_percent_drop(
+    X: anndata.AnnData,
+    ax: Axes,
+    percentList: np.ndarray,
+    runs: int,
+    rank: int = 20,
+):
+    """Plots FMS score when percentage is removed from data"""
+    dataX = pf2(X, rank, do_embedding=False)
+
+    fmsLists = []
+
+    for j in range(0, runs, 1):
+        scores = [1.0]
+
+        for i in percentList[1:]:
+            sampled_data: anndata.AnnData = sc.pp.subsample(
+                X, fraction=1 - (i / 100), random_state=j, copy=True
+            )  # type: ignore
+            sampledX = pf2(sampled_data, rank, random_state=j + 2, do_embedding=False)
+
+            fmsScore = calculateFMS(dataX, sampledX)
+            scores.append(fmsScore)
+
+        fmsLists.append(scores)
+
+    runsList_df = []
+    for i in range(0, runs):
+        for j in range(0, len(percentList)):
+            runsList_df.append(i)
+    percentList_df = []
+    for i in range(0, runs):
+        for j in range(0, len(percentList)):
+            percentList_df.append(percentList[j])
+    fmsList_df = []
+    for sublist in fmsLists:
+        fmsList_df += sublist
+    df = pd.DataFrame(
+        {
+            "Run": runsList_df,
+            "Percentage of Data Dropped": percentList_df,
+            "FMS": fmsList_df,
+        }
+    )
+
+    sns.lineplot(data=df, x="Percentage of Data Dropped", y="FMS", ax=ax)
+    ax.set_ylim(0, 1)
+
+
+def resample(data: anndata.AnnData) -> anndata.AnnData:
+    """Bootstrapping dataset"""
+    indices = np.random.randint(0, data.shape[0], size=(data.shape[0],))
+    data = data[indices].copy()
+    return data
+
+
+def plot_fms_diff_ranks(
+    X: anndata.AnnData,
+    ax: Axes,
+    ranksList: list[int],
+    runs: int,
+):
+    """Plots FMS when using different Pf2 components"""
+    fmsLists = []
+
+    for j in range(0, runs, 1):
+        scores = []
+        for i in ranksList:
+            dataX = pf2(X, rank=i, random_state=j, do_embedding=False)
+
+            sampledX = pf2(resample(X), rank=i, random_state=j, do_embedding=False)
+
+            fmsScore = calculateFMS(dataX, sampledX)
+            scores.append(fmsScore)
+        fmsLists.append(scores)
+
+    runsList_df = []
+    for i in range(0, runs):
+        for j in range(0, len(ranksList)):
+            runsList_df.append(i)
+    ranksList_df = []
+    for i in range(0, runs):
+        for j in range(0, len(ranksList)):
+            ranksList_df.append(ranksList[j])
+    fmsList_df = []
+    for sublist in fmsLists:
+        fmsList_df += sublist
+    df = pd.DataFrame(
+        {"Run": runsList_df, "Component": ranksList_df, "FMS": fmsList_df}
+    )
+
+    sns.lineplot(data=df, x="Component", y="FMS", ax=ax)
+    ax.set_ylim(0, 1)
---
+++ pf2/figures/figureA9.py
@@ -0,0 +1,250 @@
+"""
+Figure A8:
+"""
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from matplotlib.axes import Axes
+import anndata
+from .common import subplotLabel, getSetup
+from ..tensor import correct_conditions
+from .figureA4 import partial_correlation_matrix
+from typing import Tuple
+
+import pandas as pd
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.feature_selection import RFECV
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import scale
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from matplotlib.axes import Axes
+import anndata
+from .common import subplotLabel, getSetup
+from ..tensor import correct_conditions
+from .figureA4 import partial_correlation_matrix
+import numpy as np
+import pandas as pd
+from anndata import read_h5ad
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
+
+from pf2.data_import import convert_to_patients, import_meta
+from pf2.figures.common import getSetup
+# from pf2.predict import predict_mortality, run_plsr
+
+SKF = StratifiedKFold(n_splits=10)
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    ax, f = getSetup((5, 5), (1, 1))
+    subplotLabel(ax)
+    
+    X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
+    # X.uns["Pf2_A"] = correct_conditions(X)
+
+    meta = import_meta()
+    conversions = convert_to_patients(X)
+
+    patient_factor = pd.DataFrame(
+        X.uns["Pf2_A"],
+        index=conversions,
+        columns=np.arange(X.uns["Pf2_A"].shape[1]) + 1,
+    )
+    meta = meta.loc[patient_factor.index, :]
+
+
+    accuracies = pd.DataFrame(
+        columns=["Overall", "C19", "nC19"]
+    )
+
+    probabilities, labels = predict_mortality(
+        patient_factor,
+        meta,
+        proba=True
+    )
+    probabilities = probabilities.round().astype(int)
+    _meta = meta.loc[~meta.index.duplicated()].loc[labels.index]
+
+    covid_acc = accuracy_score(
+        labels.loc[_meta.loc[:, "patient_category"] == "COVID-19"],
+        probabilities.loc[_meta.loc[:, "patient_category"] == "COVID-19"]
+    )
+    nc_acc = accuracy_score(
+        labels.loc[_meta.loc[:, "patient_category"] != "COVID-19"],
+        probabilities.loc[_meta.loc[:, "patient_category"] != "COVID-19"]
+    )
+    acc = accuracy_score(labels, probabilities)
+
+    accuracies.loc[
+        0,
+        :
+        ] = [acc, covid_acc, nc_acc]
+        
+    print(accuracies)
+
+
+
+    sns.barplot(data=accuracies, ax=ax[0])
+
+    ax[0].set_ylim([0, 1])
+    ax[0].legend()
+    ax[0].set_ylabel("Accuracy")
+
+
+    
+    
+    
+
+    return f
+
+def run_plsr(
+    data: pd.DataFrame,
+    labels: pd.Series,
+    proba: bool = False,
+    n_components: int = 2
+):
+    """
+    Predicts labels via PLSR cross-validation.
+
+    Args:
+        data (pd.DataFrame): data to predict
+        labels (pd.Series): classification labels
+        proba (bool, default:False): return probability of prediction
+
+    Returns:
+        predicted (pd.Series): predicted mortality for patients; if proba is
+            True, returns probabilities of mortality
+        plsr (PLSRegression): fitted PLSR model
+    """
+    if not isinstance(data, pd.DataFrame):
+        data = pd.DataFrame(data)
+
+    data[:] = scale(data)
+    # plsr = PLSRegression(
+    #     n_components=n_components,
+    #     scale=False,
+    #     max_iter=int(1E5))
+    
+        
+    
+    # from sklearn import svm
+    # plsr = svm.SVC(kernel = "linear")
+    
+    from sklearn import svm
+    
+    from sklearn.model_selection import RandomizedSearchCV
+    from sklearn.ensemble import RandomForestRegressor
+    rf = RandomForestRegressor()
+    
+    # param_grid = {"C": [.1, 1, 10, 100, 1000],
+    #                 "gamma": [1, .1, .01, .001, .0001]}
+    
+    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+    # Number of features to consider at every split
+    # max_features = ['auto', 'sqrt']
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+    max_depth.append(None)
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4]
+    # Method of selecting samples for training each tree
+    bootstrap = [True, False]
+    # Create the random grid
+    random_grid = {'n_estimators': n_estimators,
+                # 'max_features': max_features,
+                'max_depth': max_depth,
+                'min_samples_split': min_samples_split,
+                'min_samples_leaf': min_samples_leaf,
+                'bootstrap': bootstrap}
+    plsr = RandomizedSearchCV(rf, random_grid, refit=True)
+
+        
+        
+
+    probabilities = pd.Series(0, dtype=float, index=data.index)
+    for train_index, test_index in SKF.split(data, labels):
+        train_group_data = data.iloc[train_index, :]
+        train_labels = labels.iloc[train_index]
+        test_group_data = data.iloc[test_index]
+        plsr.fit(train_group_data, train_labels)
+        probabilities.iloc[test_index] = plsr.predict(test_group_data)
+
+    plsr.fit(data, labels)
+    coef =  plsr.best_estimator_.feature_importances_.squeeze()
+    
+    if proba:
+        return probabilities, plsr, coef
+    else:
+        predicted = probabilities.round().astype(int)
+        return predicted, plsr, coef
+
+
+
+
+def predict_mortality(
+    data: pd.DataFrame,
+    meta: pd.DataFrame,
+    proba: bool = False,
+    n_components = 2
+):
+    """
+    Predicts mortality via cross-validation.
+
+    Parameters:
+        data (pd.DataFrame): data to predict
+        meta (pd.DataFrame): patient meta-data
+        proba (bool, default:False): return probability of prediction
+
+    Returns:
+        if proba:
+            probabilities (pd.Series): predicted probability of mortality for
+                patients
+            labels (pd.Series): classification targets
+        else:
+            accuracy (float): prediction accuracy
+            models (tuple[COVID, Non-COVID]): fitted PLSR models
+    """
+    if not isinstance(data, pd.DataFrame):
+        data = pd.DataFrame(data)
+
+    data = data.loc[
+        meta.loc[:, "patient_category"] != "Non-Pneumonia Control",
+        :
+    ]
+    meta = meta.loc[
+        meta.loc[:, "patient_category"] != "Non-Pneumonia Control",
+        :
+    ]
+    labels = data.index.to_series().replace(meta.loc[:, "binary_outcome"])
+
+    covid_data = data.loc[meta.loc[:, "patient_category"] == "COVID-19", :]
+    covid_labels = meta.loc[
+        meta.loc[:, "patient_category"] == "COVID-19",
+        "binary_outcome"
+    ]
+    nc_data = data.loc[meta.loc[:, "patient_category"] != "COVID-19", :]
+    nc_labels = meta.loc[
+        meta.loc[:, "patient_category"] != "COVID-19",
+        "binary_outcome"
+    ]
+
+    predictions = pd.Series(index=data.index)
+    predictions.loc[meta.loc[:, "patient_category"] == "COVID-19"], c_plsr, c_coef = \
+        run_plsr(
+            covid_data, covid_labels, proba=proba, n_components=n_components
+        )
+    predictions.loc[meta.loc[:, "patient_category"] != "COVID-19"], nc_plsr, nc_coef = \
+        run_plsr(
+            nc_data, nc_labels, proba=proba, n_components=n_components
+        )
+
+    if proba:
+        return predictions, labels
+    else:
+        predicted = predictions.round().astype(int)
+        return accuracy_score(labels, predicted), (c_plsr, c_coef, nc_plsr, nc_coef)
