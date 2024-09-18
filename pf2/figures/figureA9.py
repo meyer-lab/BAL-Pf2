7f01bd062407fd0d0419f519cebf6910d1616ef9
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-09-18 08:44:04 -0700

    Add roc auc and pred acc over components for plsr

--- pf2/figures/figureA9.py
+++ pf2/figures/figureA9.py
@@ -10,11 +10,11 @@ from ..data_import import convert_to_patients, import_meta
 from ..predict import predict_mortality
 from .common import subplotLabel, getSetup
 from sklearn.metrics import RocCurveDisplay
-
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
-    ax, f = getSetup((6, 3), (1, 2))
+    ax, f = getSetup((6, 6), (2, 2))
     subplotLabel(ax)
 
     X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
@@ -29,39 +29,57 @@ def makeFigure():
     )
     meta = meta.loc[patient_factor.index, :]
 
-    plsr_acc_df = plsr_acc_proba(patient_factor, meta)
-
-    sns.barplot(data=plsr_acc_df, ax=ax[0])
-    ax[0].set(ylim=[0, 1], ylabel="Accuracy")
     
-    plot_plsr_auc_roc(patient_factor, meta, ax[1])
+    roc_auc = [False, True]
+    for i in range(2):
+        plsr_acc_df = pd.DataFrame([])
+        for j in range(10):
+            df = plsr_acc_proba(patient_factor, meta, n_components=j+1, roc_auc=roc_auc[i])
+            df["Component"] = j+1
+            plsr_acc_df = pd.concat([plsr_acc_df, df], axis=0)
+
+        plsr_acc_df = plsr_acc_df.melt(id_vars="Component", var_name="Category", value_name="Accuracy")
+        
+        sns.lineplot(data=plsr_acc_df, x="Component", y="Accuracy", hue="Category",ax=ax[i])
+        if roc_auc is True:
+            ax[i].set(ylim=[0, 1], ylabel="AUC ROC")
+        else: 
+            ax[i].set(ylim=[0, 1], ylabel="Prediction Accuracy")
+    
+    plot_plsr_auc_roc(patient_factor, meta, ax[2])
     
     return f
     
     
-def plsr_acc_proba(patient_factor_matrix, meta_data):
+def plsr_acc_proba(patient_factor_matrix, meta_data, n_components=2, roc_auc=True):
     """Runs PLSR and obtains average prediction accuracy"""
     
     acc_df = pd.DataFrame(columns=["Overall", "C19", "nC19"])
 
     probabilities, labels = predict_mortality(
         patient_factor_matrix,
-        meta_data,
+        n_components=n_components,
+        meta=meta_data,
         proba=True
     )
 
     probabilities = probabilities.round().astype(int)
     meta_data = meta_data.loc[~meta_data.index.duplicated()].loc[labels.index]
+    
+    if roc_auc:
+        score = roc_auc_score
+    else: 
+        score = accuracy_score
 
-    covid_acc = accuracy_score(
+    covid_acc = score(
         labels.loc[meta_data.loc[:, "patient_category"] == "COVID-19"],
         probabilities.loc[meta_data.loc[:, "patient_category"] == "COVID-19"]
     )
-    nc_acc = accuracy_score(
+    nc_acc = score(
         labels.loc[meta_data.loc[:, "patient_category"] != "COVID-19"],
         probabilities.loc[meta_data.loc[:, "patient_category"] != "COVID-19"]
     )
-    acc = accuracy_score(labels, probabilities)
+    acc = score(labels, probabilities)
 
     acc_df.loc[
         0,
@@ -73,7 +91,6 @@ def plsr_acc_proba(patient_factor_matrix, meta_data):
 
 def plot_plsr_auc_roc(patient_factor_matrix, meta_data, ax):
     """Runs PLSR and plots ROC AUC based on actual and prediction labels"""
-    
     probabilities, labels = predict_mortality(
         patient_factor_matrix,
         meta_data,
