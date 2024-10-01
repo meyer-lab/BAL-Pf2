2ce36260efc492ff68c6e172f972a17c5e26e0f5
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-10-01 11:37:04 -0700

    Fix hue order

--- pf2/figures/figureA16.py
+++ pf2/figures/figureA16.py
@@ -10,7 +10,8 @@ from .common import (
 import seaborn as sns
 from pf2.figures.commonFuncs.plotGeneral import bal_combine_bo_covid
 from ..data_import import add_obs, condition_factors_meta
-
+import pandas as pd
+import numpy as np
 
 
 def makeFigure():
@@ -24,37 +25,23 @@ def makeFigure():
     cond_fact_meta_df = condition_factors_meta(X)
     cond_fact_meta_df = bal_combine_bo_covid(cond_fact_meta_df)
     pat_df = cond_fact_meta_df[["patient_id", "icu_day", "Status"]]
-    sns.violinplot(data=pat_df, x="Status", y="icu_day", hue="Status", cut=0, ax=ax[0])
+    order = np.unique(pat_df["Status"])
     
+    sns.violinplot(data=pat_df, x="Status", y="icu_day", hue="Status", hue_order=order, order=order, cut=0, ax=ax[0])
     
-    pat_df.loc[pat_df["icu_day" == 1], "Day"] = "Day1"
-    pat_df.loc[pat_df["icu_day" != 1], "Day"] = "Day>1"
-    
-                
+    pat_df["Day"] = pat_df.groupby("patient_id")["icu_day"].transform(
+        lambda x: "1TP" if x.nunique() == 1 else ("2TP" if x.nunique() == 2 else ">=3TP")
+    )
+      
     count_df = (
             pat_df.groupby(["Status", "Day"], observed=True).size().reset_index(name="Sample Count")
         )
     total = count_df["Sample Count"].sum()
     count_df["Sample Count"] = count_df["Sample Count"] / total
     
-    sns.barplot(data=count_df, x="Day", y="Sample Count", hue="Status", ax=ax[1])
-
+    sns.barplot(data=count_df, x="Day", y="Sample Count", hue="Status",ax=ax[1])
+    ax[1].set(ylabel="Sample Proportion")
     
-    for i in range(2):
-        if i == 0:
-            day_df = pat_df[pat_df["icu_day"] == 1]
-            day = "Day 1"
-        else:
-            day_df = pat_df[pat_df["icu_day"] != 1]
-            day = "Day >1"
-              
-        count_df = (
-            day_df.groupby(["Status"], observed=True).size().reset_index(name="Sample Count")
-        )
-        total = count_df["Sample Count"].sum()
-        count_df["Sample Count"] = count_df["Sample Count"] / total
-        sns.barplot(data=count_df, x="Status", y="Sample Count", hue="Status", ax=ax[i+2])
-        ax[i+2].set_title(day)
 
 
     return f
