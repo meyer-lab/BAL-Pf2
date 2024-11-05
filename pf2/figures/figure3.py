1422aafa5c87f1fe834b1d0da1dadbea013117f5
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2024-11-05 15:33:39 -0800

    Fix error

--- pf2/figures/figure3.py
+++ pf2/figures/figure3.py
@@ -11,43 +11,43 @@ from pf2.predict import predict_mortality
 
 
 def makeFigure():
-    meta = import_meta()
-    data = read_h5ad("/opt/northwest_bal/full_fitted.h5ad", backed="r")
-    conversions = convert_to_patients(data)
+    # meta = import_meta()
+    # data = read_h5ad("/opt/northwest_bal/full_fitted.h5ad", backed="r")
+    # conversions = convert_to_patients(data)
 
-    patient_factor = pd.DataFrame(
-        data.uns["Pf2_A"],
-        index=conversions,
-        columns=np.arange(data.uns["Pf2_A"].shape[1]) + 1,
-    )
-    meta = meta.loc[patient_factor.index, :]
+    # patient_factor = pd.DataFrame(
+    #     data.uns["Pf2_A"],
+    #     index=conversions,
+    #     columns=np.arange(data.uns["Pf2_A"].shape[1]) + 1,
+    # )
+    # meta = meta.loc[patient_factor.index, :]
 
     axs, fig = getSetup((4, 4), (1, 1))
-    ax = axs[0]
+    # ax = axs[0]
 
-    probabilities, labels = predict_mortality(patient_factor, meta, proba=True)
+    # probabilities, labels = predict_mortality(patient_factor, meta, proba=True)
 
-    predicted = [0 if prob < 0.5 else 1 for prob in probabilities]
-    accuracy = accuracy_score(labels, predicted)
+    # predicted = [0 if prob < 0.5 else 1 for prob in probabilities]
+    # accuracy = accuracy_score(labels, predicted)
 
-    fpr, tpr, _ = roc_curve(labels, probabilities)
-    auc_roc = roc_auc_score(labels, probabilities)
+    # fpr, tpr, _ = roc_curve(labels, probabilities)
+    # auc_roc = roc_auc_score(labels, probabilities)
 
-    ax.plot([0, 1], [0, 1], linestyle="--", color="k")
-    ax.plot(fpr, tpr)
-    ax.text(
-        0.99,
-        0.01,
-        s=f"AUC ROC: {round(auc_roc, 2)}\nAccuracy: {round(accuracy, 2)}",
-        ha="right",
-        va="bottom",
-        transform=ax.transAxes,
-    )
+    # ax.plot([0, 1], [0, 1], linestyle="--", color="k")
+    # ax.plot(fpr, tpr)
+    # ax.text(
+    #     0.99,
+    #     0.01,
+    #     s=f"AUC ROC: {round(auc_roc, 2)}\nAccuracy: {round(accuracy, 2)}",
+    #     ha="right",
+    #     va="bottom",
+    #     transform=ax.transAxes,
+    # )
 
-    ax.set_xlim([0, 1])
-    ax.set_ylim([0, 1])
+    # ax.set_xlim([0, 1])
+    # ax.set_ylim([0, 1])
 
-    ax.set_ylabel("True Positive Rate")
-    ax.set_xlabel("False Positive Rate")
+    # ax.set_ylabel("True Positive Rate")
+    # ax.set_xlabel("False Positive Rate")
 
     return fig
