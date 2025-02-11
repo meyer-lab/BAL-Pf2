8af61a23bbb6b0e06565b8e398fd4d663576416c
Andrew Ramirez <91349614+andrewram4287@users.noreply.github.com>
2025-02-10 16:42:03 -0800

    Fix s1 (#123)
    * Update S1 to current code
    
    * Use 1 plsr component
    
    ---------
    
    Co-authored-by: Andrew Ramirez <aramirez@aretha.seas.ucla.edu>

--- pf2/figures/figureS1.py
+++ pf2/figures/figureS1.py
@@ -3,40 +3,43 @@ Figure S1
 """
 
 import pandas as pd
+import numpy as np
+import anndata
 from .common import getSetup, subplotLabel
 from ..tensor import correct_conditions
-import numpy as np
-from pf2.data_import import import_data, condition_factors_meta
-from pf2.figures.common import getSetup
-from pf2.predict import predict_mortality_all
-from pf2.tensor import pf2
+from ..data_import import condition_factors_meta
+from ..figures.common import getSetup
+from ..predict import predict_mortality_all
+from ..tensor import pf2
 
 
 def makeFigure():
     ax, f = getSetup((6, 6), (2, 2))
     subplotLabel(ax)
     
-    # X = import_data()
-    # ranks = np.arange(5, 65, 5)
-    # ranks = [2, 5]
-    # r2xs = pd.Series(0, dtype=float, index=ranks)
-    # accuracies = pd.Series(0, dtype=float, index=ranks)
-    # for rank in ranks:
-    #     XX, r2x = pf2(X, rank, do_embedding=False)
-    #     XX.uns["Pf2_A"] = correct_conditions(XX)
-    #     cond_fact_meta_df = condition_factors_meta(XX)
-    #     acc, _, _ = predict_mortality_all(XX, cond_fact_meta_df, 
-    #                                         n_components=2, proba=True)
-    #     r2xs.loc[rank] = r2x
-    #     accuracies.loc[rank] = acc
+    X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
+    X.obs["condition_unique_idxs"] = pd.Categorical(X.obs["condition_unique_idxs"])
+    ranks = np.arange(5, 70, 5)
+    ranks = [2]
+    r2xs = pd.Series(0, dtype=float, index=ranks)
+    accuracies = pd.Series(0, dtype=float, index=ranks)
     
-    # ax[0].plot(ranks, r2xs)
-    # ax[0].set(xticks = ranks, ylabel = "R2X", xlabel = "Rank")
-    # ax[1].plot(ranks, accuracies,)
-    # ax[1].set(xticks = ranks, ylabel = "Accuracy", xlabel = "Rank")
+    for rank in ranks:
+        XX, r2x = pf2(X, rank, do_embedding=False)
+        XX.uns["Pf2_A"] = correct_conditions(XX)
+        cond_fact_meta_df = condition_factors_meta(XX)
+        acc, _, _ = predict_mortality_all(XX, cond_fact_meta_df, 
+                                            n_components=1, proba=False)
+        r2xs.loc[rank] = r2x
+        accuracies.loc[rank] = acc
     
-    # ax[0].set(xticks=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65])
-    # ax[1].set(xticks=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+    ax[0].scatter(ranks, r2xs)
+    ax[0].set(xticks = ranks, ylabel = "R2X", xlabel = "Rank")
+    ax[1].scatter(ranks, accuracies,)
+    ax[1].set(xticks = ranks, ylabel = "Accuracy", xlabel = "Rank")
+    
+    ax[0].set(xticks=[0, 10, 20, 30, 40, 50, 60, 70])
+    ax[1].set(xticks=[0, 10, 20, 30, 40, 50, 60, 70])
 
 
     return f
