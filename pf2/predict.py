17d112046ed024d3a0e7bae1bf56ff5b8b59a90b
Jackson Chin <JacksonLymanChin@gmail.com>
2025-03-17 12:45:38 -0700

    Adds grouped cross-validation to PLSR predict

--- pf2/predict.py
+++ pf2/predict.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import accuracy_score, roc_auc_score
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedGroupKFold, StratifiedKFold
 import anndata
 SKF = StratifiedKFold(n_splits=10)
+SGKF = StratifiedGroupKFold(n_splits=10)
 
 
 def run_plsr(
-    data: pd.DataFrame, labels: pd.Series, proba: bool = False, n_components: int = 1
+    data: pd.DataFrame,
+    labels: pd.DataFrame,
+    proba: bool = False,
+    n_components: int = 1
 ) -> tuple[pd.Series, PLSRegression]:
     """
     Predicts labels via PLSR cross-validation.
@@ -17,6 +21,7 @@ def run_plsr(
         data (pd.DataFrame): data to predict
         labels (pd.Series): classification labels
         proba (bool, default:False): return probability of prediction
+        n_components (int, default:1): number of PLSR components to use
 
     Returns:
         predicted (pd.Series): predicted mortality for patients; if proba is
@@ -29,14 +34,18 @@ def run_plsr(
     plsr = PLSRegression(n_components=n_components, scale=True, max_iter=int(1e5))
 
     probabilities = pd.Series(0, dtype=float, index=data.index)
-    for train_index, test_index in SKF.split(data, labels):
+    for train_index, test_index in SGKF.split(
+        data,
+        labels.loc[:, "binary_outcome"],
+        labels.loc[:, "patient_id"]
+    ):
         train_group_data = data.iloc[train_index, :]
-        train_labels = labels.iloc[train_index]
+        train_labels = labels.iloc[train_index].loc[:, "binary_outcome"]
         test_group_data = data.iloc[test_index]
         plsr.fit(train_group_data, train_labels)
         probabilities.iloc[test_index] = plsr.predict(test_group_data)
 
-    plsr.fit(data, labels)
+    plsr.fit(data, labels.loc[:, "binary_outcome"])
     plsr.coef_ = pd.Series(plsr.coef_.squeeze(), index=data.columns)
 
     if proba:
@@ -73,8 +82,7 @@ def predict_mortality_all(
 
     cond_fact_meta_df = data[data["patient_category"] != "Non-Pneumonia Control"]
     
-    labels = cond_fact_meta_df["binary_outcome"]
-    labels = pd.Series(index=labels.index, data=labels.to_numpy().astype(int))
+    labels = cond_fact_meta_df.loc[:, ["binary_outcome", "patient_id"]]
     predictions = pd.Series(index=cond_fact_meta_df.index)
     
     if bulk is False:
@@ -84,16 +92,20 @@ def predict_mortality_all(
         )
     else:
         predictions[:], all_plsr = run_plsr(
-            cond_fact_meta_df.iloc[:, :-2], 
+            cond_fact_meta_df.iloc[:, :-3],
             labels, proba=proba, n_components=n_components
         )
         
     if proba:
-        return predictions, labels
+        return predictions, labels.loc[:, "binary_outcome"]
 
     else:
         predicted = predictions.round().astype(int)
-        return  accuracy_score(labels, predicted), labels, all_plsr
+        return (
+            accuracy_score(labels.loc[:, "binary_outcome"], predicted),
+            labels.loc[:, "binary_outcome"],
+            all_plsr
+        )
     
 
 def predict_mortality(
@@ -120,18 +132,25 @@ def predict_mortality(
     if not isinstance(data, pd.DataFrame):
         data = pd.DataFrame(data)
 
-    cond_fact_meta_df = data[data["patient_category"] != "Non-Pneumonia Control"]
+    cond_fact_meta_df = data.loc[
+        data.loc[:, "patient_category"] != "Non-Pneumonia Control",
+        :
+    ]
     
-    labels = cond_fact_meta_df["binary_outcome"]
-    labels = pd.Series(index=labels.index, data=labels.to_numpy().astype(int))
+    labels = cond_fact_meta_df.loc[
+         :,
+         ["binary_outcome", "patient_id"]
+     ].astype(int)
 
     covid_data = cond_fact_meta_df.loc[cond_fact_meta_df.loc[:, "patient_category"] == "COVID-19", :]
-    covid_labels = cond_fact_meta_df.loc[
-        cond_fact_meta_df.loc[:, "patient_category"] == "COVID-19", "binary_outcome"
+    covid_labels = labels.loc[
+        cond_fact_meta_df.loc[:, "patient_category"] == "COVID-19",
+        :
     ]
     nc_data = cond_fact_meta_df.loc[cond_fact_meta_df.loc[:, "patient_category"] != "COVID-19", :]
-    nc_labels = cond_fact_meta_df.loc[
-        cond_fact_meta_df.loc[:, "patient_category"] != "COVID-19", "binary_outcome"
+    nc_labels = labels.loc[
+        cond_fact_meta_df.loc[:, "patient_category"] != "COVID-19",
+        :
     ]
 
     predictions = pd.Series(index=cond_fact_meta_df.index)
@@ -144,7 +163,11 @@ def predict_mortality(
         nc_labels, proba=proba, n_components=n_components
     )
 
-    return predictions, labels, (c_plsr, nc_plsr)
+    return (
+        predictions,
+        labels.loc[:, "binary_outcome"].squeeze(),
+        (c_plsr, nc_plsr)
+    )
 
 
 def plsr_acc_proba(X, patient_factor_matrix, n_components=1, roc_auc=True):
