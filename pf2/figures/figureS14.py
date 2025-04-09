e4781fefa81c939f5ba099a4b259f68785818109
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2025-04-09 11:22:12 -0700

    Add plot that investigates coexpression

---
+++ pf2/figures/figureS14.py
@@ -0,0 +1,91 @@
+"""Figure 14"""
+
+import anndata
+import scanpy as sc
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from .commonFuncs.plotGeneral import rotate_xaxis
+
+import numpy as np
+import anndata
+from .common import subplotLabel, getSetup
+from .commonFuncs.plotGeneral import rotate_xaxis, plot_avegene_cmps, plot_pair_gene_factors, plot_avegene_scatter_cmps
+from ..data_import import add_obs, combine_cell_types
+from ..utilities import bot_top_genes, add_obs_cmp_both_label, add_obs_cmp_unique_two
+from .commonFuncs.plotPaCMAP import plot_gene_pacmap, plot_labels_pacmap, plot_wp_pacmap
+import matplotlib.colors as mcolors
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    ax, f = getSetup((10, 10), (4, 4))
+
+    subplotLabel(ax)
+
+    X = anndata.read_h5ad("/opt/northwest_bal/full_fitted.h5ad")
+    add_obs(X, "binary_outcome")
+    add_obs(X, "patient_category")
+    X = X[X.obs["patient_category"] != "Non-Pneumonia Control"] 
+    combine_cell_types(X)
+    
+    cmp1 = 55; cmp2 = 67
+    pos1 = True; pos2 = True
+    
+    threshold = 0.1
+    X = add_obs_cmp_both_label(X, cmp1, cmp2, pos1, pos2, top_perc=threshold)
+    X = add_obs_cmp_unique_two(X, cmp1, cmp2)
+
+    # genes1 = ["SFN"]
+    genes1 = ["SCGB3A2"]
+    
+    marker_genes = ["SCGB1A1", "FOXJ1", "SCGB3A1", "DNAH5", "TUBA1A", "MUC5AC", "MUC5B", "AGER", "SFTPC", "HOPX", "ABCA3", "PECAM1", "AGR2"]
+
+    X = X[X.obs["Label"] != "Both"] 
+    
+    for i, geneA in enumerate(genes1):
+        for j, geneB in enumerate(marker_genes):
+            genes = np.concatenate([[geneA], [geneB]])
+            plot_avegene_scatter_cmps(X, genes, ax[j])
+    
+        
+    return f
+
+  
+
+def score(X: anndata.AnnData,  genes):
+    """Scanpy average gene score for all cells"""
+    X = sc.tl.score_genes(adata=X, gene_list=genes, copy=True, use_raw=False)
+
+    return X
+
+
+def plot_score(X: anndata.AnnData, ax, cellType: str = "Label"):
+    """Plots average score  across cell types and patient status"""
+    df = pd.DataFrame({"Score": X.obs["score"].values})
+    df["Condition"] = X.obs["sample_id"].values
+    df["Label"] = X.obs[cellType].values
+    df_mean_score = (
+        df.groupby(["Label", "Condition"], observed=False)
+        .mean()
+        .reset_index()
+        .sort_values(["Label", "Condition"])
+    )
+    df_count = (
+        df.groupby(["Label", "Condition"], observed=False)
+        .size()
+        .reset_index(name="Cell Count")
+        .sort_values(["Label", "Condition"])
+    )
+    df_mean_score["Cell Count"] = df_count["Cell Count"].to_numpy()
+
+    sns.boxplot(
+        data=df_mean_score,
+        x="Label",
+        y="Score",
+        order=np.unique(df["Label"]),
+        ax=ax,
+        showfliers=False,
+    )
+
+    rotate_xaxis(ax)
